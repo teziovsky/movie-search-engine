581be84ca8869ef70a97c26e55372b3d3211de3c
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-06 18:44:27 +0200

    Created CategoryCard component

---
+++ src/components/CategoryCard/CategoryCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card my-3">
+        <h5 class="card-header">{{ title }}</h5>
+        <div class="card-body d-flex flex-wrap justify-content-center">
+            <MovieCard />
+        </div>
+    </div>
+</template>
+
+<script>
+import MovieCard from "./MovieCard";
+export default {
+    props: ["title"],
+    name: "CategoryCard",
+    components: {
+        MovieCard,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
