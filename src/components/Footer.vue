0ec137b0089c593d8780e34eac0c3d88b4f6a24e
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-06 18:44:14 +0200

    Created footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="copyright">
+        © 2020 Copyright:
+        <a href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+.copyright {
+    padding: 15px 0;
+    background-color: #f8f9fa;
+    border-top: 1px solid #dadbdd;
+    margin-top: 20px;
+    text-align: center;
+}
+</style>
