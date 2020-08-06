645c339e81e87081e450beeafcdf255d4f87d8c4
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-06 18:44:52 +0200

    Created MovieCard component

---
+++ src/components/CategoryCard/MovieCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card mx-2" style="width: 100px; height: auto">
+        <img src="../../assets/2.jpg" class="card-img-top" alt="#" />
+        <div class="card-body">
+            <p class="card-title small text-center m-0">Card title</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MovieCard",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
