38473f2f28c556051fe11a09bc297cccd4cde8a2
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-07 22:27:08 +0200

    Added active link style and methods to search movies

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,10 +15,10 @@
         <div class="justify-content-end collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item">
-                    <router-link to="/" class="nav-link" exact active-class="text-bold">Home</router-link>
+                    <router-link to="/" class="nav-link" exact>Home</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link to="/movie" class="nav-link" active-class="text-bold">Movies</router-link>
+                    <router-link to="/movies" class="nav-link">Movies</router-link>
                 </li>
             </ul>
             <form class="d-flex align-center my-2 my-lg-0 mr-lg-5">
@@ -27,8 +27,13 @@
                     type="search"
                     placeholder="Search"
                     aria-label="Search"
+                    v-model.lazy="message"
                 />
-                <button class="btn btn-outline-success my-sm-0" type="submit">Search</button>
+                <button
+                    class="btn btn-outline-success my-sm-0"
+                    type="submit"
+                    @click.prevent="search"
+                >Search</button>
             </form>
         </div>
     </nav>
@@ -37,11 +42,37 @@
 <script>
 export default {
     name: "Navbar",
+    data() {
+        return {
+            // message: "",
+        };
+    },
+    computed: {
+        message: {
+            get() {
+                return this.$store.state.searchQuery;
+            },
+            set(value) {
+                this.$store.commit("FETCH_SEARCHQUERY", value);
+                this.$store.dispatch("fetchSearchResults", value);
+            },
+        },
+    },
+    methods: {
+        search(e) {
+            this.$store.dispatch("fetchSearchResults", e.target.form[0].value);
+            console.log(e.target.form[0].value);
+        },
+    },
 };
 </script>
 
 <style lang="scss" scoped>
 .border-b {
     border-bottom: 1px solid #dadbdd;
+}
+
+.router-link-active {
+    font-weight: 700;
 }
 </style>
