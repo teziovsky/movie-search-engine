b449fe9b6fda759f6ac5627eb7a5d2263b8b8559
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-07 22:29:22 +0200

    Added logic to render components depends on data passed with props. Changed justify of items in component

--- src/components/CategoryCard/CategoryCard.vue
+++ src/components/CategoryCard/CategoryCard.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="card my-3">
         <h5 class="card-header">{{ title }}</h5>
-        <div class="card-body d-flex flex-wrap justify-content-center">
-            <MovieCard />
+        <div class="card-body d-flex flex-wrap justify-content-around">
+            <MovieCard v-for="movie in this.$attrs.category" :movie="movie" :key="movie.id" />
         </div>
     </div>
 </template>
 
 <script>
 import MovieCard from "./MovieCard";
+
 export default {
-    props: ["title"],
+    props: {
+        title: {
+            type: String,
+        },
+    },
     name: "CategoryCard",
     components: {
         MovieCard,
