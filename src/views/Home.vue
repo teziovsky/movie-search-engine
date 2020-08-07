03a302218cad9678b2fb16fa3b5be040c0e04caa
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-07 22:33:14 +0200

    Added logic to render CategoryCard components depends on data in props. Added computed properties and created hook to dispatch 'Now Playing', 'Most Popular' and 'Top Rated' films

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div class="jumbotron m-0 text-center">
+            <h1 class="display-4">Movies search!</h1>
+            <p
+                class="lead"
+            >This is a simple website to search movies from The movies DB API. Created for the needs of recruitment to Junior Frontend Developer.</p>
+        </div>
+        <CategoryCard
+            title="Search Results"
+            :category="searchResults.results"
+            v-if="searchResults.length!=0"
+        />
+        <CategoryCard
+            v-else
+            v-for="title in titles"
+            :key="title"
+            :title="title"
+            :category="title=='Now Playing' ? nowPlaying : title=='Most Popular' ? mostPopular : title=='Top Rated' ? topRated : null"
+        />
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import { mapState } from "vuex";
+import CategoryCard from "../components/CategoryCard/CategoryCard";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+    name: "Home",
+    data() {
+        return {
+            titles: ["Now Playing", "Most Popular", "Top Rated"],
+        };
+    },
+    computed: {
+        ...mapState(["nowPlaying", "mostPopular", "topRated", "searchResults"]),
+    },
+    created() {
+        this.$store.dispatch("fetchNowPlaying");
+        this.$store.dispatch("fetchMostPopular");
+        this.$store.dispatch("fetchTopRated");
+    },
+    components: {
+        CategoryCard,
+    },
 };
 </script>
+
+<style lang="scss" scoped>
+</style>
