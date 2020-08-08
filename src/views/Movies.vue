b1bf4f4f37990199ea866d98dea7ffe20cf722b4
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-08 16:13:03 +0200

    Added Movies.vue view component with logic to fetch and sort movies at movies list

---
+++ src/views/Movies.vue
@@ -0,0 +1,105 @@
+<template>
+    <div>
+        <div class="jumbotron m-0 p-4 text-center">
+            <h1 class="display-4">List of movies</h1>
+        </div>
+        <div class="select">
+            <label for="select">Sort by:</label>
+            <select id="select" class="custom-select" v-model="sortBy">
+                <option value="popularity.desc" selected>Popularity ↓</option>
+                <option value="popularity.asc">Popularity ↑</option>
+                <option value="original_title.desc">Original title ↓</option>
+                <option value="original_title.asc">Original title ↑</option>
+            </select>
+        </div>
+        <div class="d-flex flex-wrap justify-content-center">
+            <MovieTile v-for="movie in showMovies" :movie="movie" :key="movie.id" />
+        </div>
+        <Pagination :page="page" :lastPage="lastPage" :prevPage="prevPage" :nextPage="nextPage" />
+    </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import MovieTile from "../components/MovieTile";
+import Pagination from "../components/Pagination";
+export default {
+    name: "Movies",
+    data() {
+        return {
+            page: 1,
+            perPage: 20,
+            loading: true,
+        };
+    },
+    computed: {
+        ...mapState(["allMovies"]),
+        showMovies() {
+            let start = (this.page - 1) * this.perPage;
+            let end = start + this.perPage;
+            return this.allMovies.slice(start, end);
+        },
+        lastPage() {
+            return this.$store.state.lastPageNr;
+        },
+        sortBy: {
+            get() {
+                return this.$store.state.sortBy;
+            },
+            set(value) {
+                this.$store.commit("SET_SORTBY", value);
+            },
+        },
+    },
+    methods: {
+        prevPage() {
+            this.page--;
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        },
+        nextPage() {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+            this.page++;
+            this.$store.dispatch("fetchNextPage", this.page, this.sortBy);
+        },
+    },
+    created() {
+        this.$store.dispatch("fetchAllMovies", this.page, this.sortBy);
+    },
+    watch: {
+        sortBy() {
+            this.$store.dispatch("fetchAllMovies", this.page, this.sortBy);
+            this.page = 1;
+        },
+    },
+    components: {
+        MovieTile,
+        Pagination,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.img-height {
+    height: auto;
+    width: inherit;
+}
+
+.card {
+    width: 300px;
+    margin: 10px;
+}
+
+.select {
+    display: flex;
+    flex: column;
+    align-items: baseline;
+    justify-content: center;
+    margin-top: 1em;
+    margin-bottom: 5px;
+}
+
+.custom-select {
+    width: 200px;
+    margin-left: 5px;
+}
+</style>
