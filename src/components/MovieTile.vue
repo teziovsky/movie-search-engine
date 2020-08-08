abcd4eddd60053d6e4939ebd81d9c14915519c4f
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-08 18:31:47 +0200

    Added logic of default image in img tag and styles

--- src/components/MovieTile.vue
+++ src/components/MovieTile.vue
@@ -1,23 +1,24 @@
 <template>
     <router-link :to="'/movie/' + movie.id" class="card">
         <div class="row no-gutters">
-            <div class="col-5">
+            <div class="col">
                 <img
                     :src="'http://image.tmdb.org/t/p/w500/' + movie.poster_path"
+                    @error="defaultImg"
                     class="card-img img-height"
-                    alt="test-alt"
+                    :alt="movie.title + ' poster picture'"
                 />
             </div>
-            <div class="col-7">
+            <div class="col">
                 <div
-                    class="h-100 card-body d-flex flex-column justify-content-between align-items-center"
+                    class="h-100 card-body d-flex flex-column justify-content-between align-items-center p-3"
                 >
-                    <h5 class="mt-2 card-title">{{movie.title}}</h5>
+                    <h6 class="mt-1 card-title">{{movie.title}}</h6>
                     <div>
-                        <p class="card-text">
+                        <p class="card-text m-0">
                             <small>Popularity: {{movie.popularity}}</small>
                         </p>
-                        <p class="card-text=">
+                        <p class="card-text m-0">
                             <small>Votes: {{movie.vote_count}}</small>
                         </p>
                     </div>
@@ -30,7 +31,31 @@
 export default {
     props: ["movie"],
     name: "MovieTile",
+    data() {
+        return {
+            defaultImage: require("@/assets/no-img.jpg"),
+        };
+    },
+    methods: {
+        defaultImg(e) {
+            e.target.src = this.defaultImage;
+        },
+    },
 };
 </script>
 <style lang="scss" scoped>
+a {
+    color: #212529;
+    text-decoration: none;
+}
+.card {
+    width: 300px;
+    margin: 10px;
+}
+.img-height {
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    width: inherit;
+}
 </style>
