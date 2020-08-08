097095790a92e5b53cb46b9f1a4b9ffb1b8e8d2a
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-08 16:11:08 +0200

    Reduced padding of jumbotron, deleted search results CategoryCard, added mapState and changed created hook to mounted

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
     <div>
-        <div class="jumbotron m-0 text-center">
+        <div class="jumbotron m-0 p-4 text-center">
             <h1 class="display-4">Movies search!</h1>
             <p
                 class="lead"
             >This is a simple website to search movies from The movies DB API. Created for the needs of recruitment to Junior Frontend Developer.</p>
         </div>
         <CategoryCard
-            title="Search Results"
-            :category="searchResults.results"
-            v-if="searchResults.length!=0"
-        />
-        <CategoryCard
-            v-else
             v-for="title in titles"
             :key="title"
             :title="title"
@@ -33,9 +27,9 @@ export default {
         };
     },
     computed: {
-        ...mapState(["nowPlaying", "mostPopular", "topRated", "searchResults"]),
+        ...mapState(["nowPlaying", "mostPopular", "topRated"]),
     },
-    created() {
+    mounted() {
         this.$store.dispatch("fetchNowPlaying");
         this.$store.dispatch("fetchMostPopular");
         this.$store.dispatch("fetchTopRated");
