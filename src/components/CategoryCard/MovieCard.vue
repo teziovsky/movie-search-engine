54014429bf9ce35cb58b153ee94c8f09a410eaf4
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-08 16:16:16 +0200

    Added styles and view of MovieCard

--- src/components/CategoryCard/MovieCard.vue
+++ src/components/CategoryCard/MovieCard.vue
@@ -1,17 +1,45 @@
 <template>
-    <div class="card mx-2" style="width: 100px; height: auto">
-        <img src="../../assets/2.jpg" class="card-img-top" alt="#" />
-        <div class="card-body">
-            <p class="card-title small text-center m-0">Card title</p>
+    <router-link
+        :to="'/movie/' + movie.id"
+        class="card mx-1 my-1"
+        style="width: 110px; height: auto"
+    >
+        <img
+            :src="'http://image.tmdb.org/t/p/w500/'+ movie.poster_path"
+            class="card-img-top"
+            :alt="movie.title + ' picture'"
+        />
+        <div class="card-body d-flex align-items-center justify-content-center py-1 px-1">
+            <p class="title small">{{movie.title}}</p>
         </div>
-    </div>
+    </router-link>
 </template>
 
 <script>
 export default {
+    props: ["movie"],
     name: "MovieCard",
 };
 </script>
 
 <style lang="scss" scoped>
+a {
+    color: #212529;
+    text-decoration: none;
+
+    &:hover {
+        border: 1px solid rgba($color: #000000, $alpha: 0.4);
+
+        & .title {
+            text-shadow: 0.5px 0px 0px rgba($color: #000000, $alpha: 0.5);
+        }
+    }
+}
+
+.title {
+    font-size: 80%;
+    font-weight: 400;
+    text-align: center;
+    margin: 0;
+}
 </style>
