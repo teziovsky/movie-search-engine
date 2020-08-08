dec58e6b05a45dd4d71ae32fbcbab7f8ddc5773b
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-08 15:34:24 +0200

    Added MovieTile component. The template of each movie tile

---
+++ src/components/MovieTile.vue
@@ -0,0 +1,36 @@
+<template>
+    <router-link :to="'/movie/' + movie.id" class="card">
+        <div class="row no-gutters">
+            <div class="col-5">
+                <img
+                    :src="'http://image.tmdb.org/t/p/w500/' + movie.poster_path"
+                    class="card-img img-height"
+                    alt="test-alt"
+                />
+            </div>
+            <div class="col-7">
+                <div
+                    class="h-100 card-body d-flex flex-column justify-content-between align-items-center"
+                >
+                    <h5 class="mt-2 card-title">{{movie.title}}</h5>
+                    <div>
+                        <p class="card-text">
+                            <small>Popularity: {{movie.popularity}}</small>
+                        </p>
+                        <p class="card-text=">
+                            <small>Votes: {{movie.vote_count}}</small>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </router-link>
+</template>
+<script>
+export default {
+    props: ["movie"],
+    name: "MovieTile",
+};
+</script>
+<style lang="scss" scoped>
+</style>
