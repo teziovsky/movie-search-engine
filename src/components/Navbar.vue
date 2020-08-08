eb9de5e8f27a4988046449bb792e30e279eaada5
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-08 16:08:32 +0200

    Deleted search form, and logic to search.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,23 +18,12 @@
                     <router-link to="/" class="nav-link" exact>Home</router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link to="/movies" class="nav-link">Movies</router-link>
+                    <router-link to="/movies/" class="nav-link">List Of Movies</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/search" class="nav-link">Search</router-link>
                 </li>
             </ul>
-            <form class="d-flex align-center my-2 my-lg-0 mr-lg-5">
-                <input
-                    class="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Search"
-                    aria-label="Search"
-                    v-model.lazy="message"
-                />
-                <button
-                    class="btn btn-outline-success my-sm-0"
-                    type="submit"
-                    @click.prevent="search"
-                >Search</button>
-            </form>
         </div>
     </nav>
 </template>
@@ -42,28 +31,6 @@
 <script>
 export default {
     name: "Navbar",
-    data() {
-        return {
-            // message: "",
-        };
-    },
-    computed: {
-        message: {
-            get() {
-                return this.$store.state.searchQuery;
-            },
-            set(value) {
-                this.$store.commit("FETCH_SEARCHQUERY", value);
-                this.$store.dispatch("fetchSearchResults", value);
-            },
-        },
-    },
-    methods: {
-        search(e) {
-            this.$store.dispatch("fetchSearchResults", e.target.form[0].value);
-            console.log(e.target.form[0].value);
-        },
-    },
 };
 </script>
 
