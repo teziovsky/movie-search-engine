3331f3f209cc70200789535bfc3f47a90a705ac8
Jakub Soboczyński <teziovsky@gmail.com>
2020-08-10 22:06:06 +0200

    Updated vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/movie-search-engine/' : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/movie-search-engine/" : "/"
 };
